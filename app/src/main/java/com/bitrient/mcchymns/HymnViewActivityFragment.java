e5b42258ca5c26cfbc67fca2d55b729f51609cd2
Cinfwat Probity <czprobity@gmail.com>
2015-06-26 00:17:35 +0100

    Initial commit of the HymnView layout files

---
+++ app/src/main/java/com/bitrient/mcchymns/HymnViewActivityFragment.java
@@ -0,0 +1,23 @@
+package com.bitrient.mcchymns;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class HymnViewActivityFragment extends Fragment {
+
+    public HymnViewActivityFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_hymn_view, container, false);
+    }
+}
