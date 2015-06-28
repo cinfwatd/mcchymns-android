899503d029b6f16b121cce2f320da742934a3692
Cinfwat Probity <czprobity@gmail.com>
2015-06-28 01:04:28 +0100

    Add custom keypad to the GotoHymn Dialog for easier and user friendly experience

--- app/src/main/java/com/bitrient/mcchymns/fragment/dialog/GotoHymnDialogFragment.java
+++ app/src/main/java/com/bitrient/mcchymns/fragment/dialog/GotoHymnDialogFragment.java
@@ -1,18 +1,20 @@
 package com.bitrient.mcchymns.fragment.dialog;
 
 import android.app.Dialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AlertDialog;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.NumberPicker;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bitrient.mcchymns.HymnViewActivity;
 import com.bitrient.mcchymns.R;
@@ -21,7 +23,7 @@ import com.bitrient.mcchymns.R;
  * @author Cinfwat Probity <czprobity@bitrient.com>
  * @since 6/24/15
  */
-public class GotoHymnDialogFragment extends DialogFragment {
+public class GotoHymnDialogFragment extends DialogFragment implements View.OnClickListener{
 
     public static GotoHymnDialogFragment newInstance() {
         return new GotoHymnDialogFragment();
@@ -32,7 +34,7 @@ public class GotoHymnDialogFragment extends DialogFragment {
 
     View mDialogHeader;
     LinearLayout mDialogView;
-    NumberPicker mNumberPicker;
+    EditText mSelectedHymnTextView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -51,15 +53,71 @@ public class GotoHymnDialogFragment extends DialogFragment {
         TextView dialogTitle = (TextView) mDialogHeader.findViewById(R.id.dialog_title);
         dialogTitle.setText(R.string.select_hymn_number);
 
-        mDialogView = (LinearLayout) inflater.inflate(R.layout.dialog_number_picker, null);
-        mNumberPicker = (NumberPicker) mDialogView.findViewById(R.id.dialog_number_picker);
+        mDialogView = (LinearLayout) inflater.inflate(R.layout.dialog_keypad, null);
+        mSelectedHymnTextView = (EditText) mDialogView.findViewById(R.id.selected_hymn);
 
-        mNumberPicker.setMinValue(1);
-        mNumberPicker.setMaxValue(1200);
+        Button oneBtn = (Button) mDialogView.findViewById(R.id.btn_one);
+        oneBtn.setOnClickListener(this);
+        Button twoBtn = (Button) mDialogView.findViewById(R.id.btn_two);
+        twoBtn.setOnClickListener(this);
+        Button threeBtn = (Button) mDialogView.findViewById(R.id.btn_three);
+        threeBtn.setOnClickListener(this);
+        Button fourBtn = (Button) mDialogView.findViewById(R.id.btn_four);
+        fourBtn.setOnClickListener(this);
+        Button fiveBtn = (Button) mDialogView.findViewById(R.id.btn_five);
+        fiveBtn.setOnClickListener(this);
+        Button sixBtn = (Button) mDialogView.findViewById(R.id.btn_six);
+        sixBtn.setOnClickListener(this);
+        Button sevenBtn = (Button) mDialogView.findViewById(R.id.btn_seven);
+        sevenBtn.setOnClickListener(this);
+        Button eightBtn = (Button) mDialogView.findViewById(R.id.btn_eight);
+        eightBtn.setOnClickListener(this);
+        Button nineBtn = (Button) mDialogView.findViewById(R.id.btn_nine);
+        nineBtn.setOnClickListener(this);
+        Button zeroBtn = (Button) mDialogView.findViewById(R.id.btn_zero);
+        zeroBtn.setOnClickListener(this);
+
+        Button acceptBtn = (Button) mDialogView.findViewById(R.id.btn_accept);
+        Button declineBtn = (Button) mDialogView.findViewById(R.id.btn_cancel);
+
+        Button clearBtn = (Button) mDialogView.findViewById(R.id.btn_clear);
 
         if (savedInstanceState != null) {
-            mNumberPicker.setValue(savedInstanceState.getInt(HymnViewActivity.SELECTED_HYMN));
+            mSelectedHymnTextView.setText(savedInstanceState.getString(HymnViewActivity.SELECTED_HYMN));
         }
+
+        declineBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dismiss();
+            }
+        });
+
+        acceptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (!TextUtils.isEmpty(mSelectedHymnTextView.getText())
+                        && (Integer.parseInt(String.valueOf(mSelectedHymnTextView.getText())) > 0)) {
+                    Intent hymnIntent = new Intent(getActivity(), HymnViewActivity.class);
+                    hymnIntent.putExtra(HymnViewActivity.SELECTED_HYMN, Integer.parseInt(String.valueOf(mSelectedHymnTextView.getText())));
+                    startActivity(hymnIntent);
+                } else {
+                    Toast.makeText(getActivity(),
+                            getResources().getString(R.string.empty_hymn_number), Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+        clearBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String value = mSelectedHymnTextView.getText().toString();
+                int length = value.length();
+                if (length >= 1) mSelectedHymnTextView.setText(TextUtils.substring(value, 0, length -1));
+            }
+        });
     }
 
     @NonNull
@@ -70,32 +128,23 @@ public class GotoHymnDialogFragment extends DialogFragment {
 //        builder.setTitle(getActivity().getResources().getString(R.string.select_hymn_number));
         builder.setCustomTitle(mDialogHeader);
         builder.setView(mDialogView);
-        builder.setPositiveButton(getActivity().getResources().getString(R.string.open_hymn),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        int selectedHymn = mNumberPicker.getValue();
-
-                        Intent hymnIntent = new Intent(getActivity(), HymnViewActivity.class);
-                        hymnIntent.putExtra(HymnViewActivity.SELECTED_HYMN, selectedHymn);
-                        startActivity(hymnIntent);
-                    }
-                });
-
-        builder.setNegativeButton(getActivity().getResources().getString(R.string.confirm_decline),
-                new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.cancel();
-                    }
-                });
-
         return builder.create();
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
-        outState.putInt(HymnViewActivity.SELECTED_HYMN, mNumberPicker.getValue());
+        outState.putString(HymnViewActivity.SELECTED_HYMN,  mSelectedHymnTextView.getText().toString());
         super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView clickedBtn = (TextView) v;
+        String value = String.valueOf(clickedBtn.getText());
+        String curValue = String.valueOf(mSelectedHymnTextView.getText());
+
+        int newValue = Integer.parseInt(String.format("%s%s", curValue, value));
+
+        if (newValue > 0 && newValue <= 1200) mSelectedHymnTextView.setText(Integer.toString(newValue));
     }
 }
