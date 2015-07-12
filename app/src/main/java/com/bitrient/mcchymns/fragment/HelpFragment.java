4bf7a24792465c91591b8e61ae9f9cff55099966
Cinfwat Probity <czprobity@gmail.com>
2015-07-13 00:31:57 +0100

    Add the About dialog, barebones Feedback and the Help fragment footer

--- app/src/main/java/com/bitrient/mcchymns/fragment/HelpFragment.java
+++ app/src/main/java/com/bitrient/mcchymns/fragment/HelpFragment.java
@@ -1,14 +1,17 @@
 package com.bitrient.mcchymns.fragment;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.bitrient.mcchymns.R;
+import com.bitrient.mcchymns.fragment.dialog.AboutDialogFragment;
 
 
 /**
@@ -39,5 +42,25 @@ public class HelpFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_help, container, false);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.about:
+//                Intent aboutIntent = new Intent(getActivity(), AboutActivity.class);
+//                startActivity(aboutIntent);
+                AboutDialogFragment aboutFragment = new AboutDialogFragment();
+                aboutFragment.show(getFragmentManager(), "about");
+                break;
+            case R.id.feedback:
+                Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                emailIntent.setType("text/email");
+                emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[]{"mcchymns@bitrient.com"});
+                emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Feedback");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, "Dear bitrient ..");
 
+                startActivity(Intent.createChooser(emailIntent, "Send Feedback"));
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
